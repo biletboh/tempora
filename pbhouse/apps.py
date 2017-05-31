422591f38e928466881300d4ecef0bcb1545b276
biletboh <biletskyboh@gmail.com>
2017-05-31 16:14:44 +0300

    added user profile to models, created signals, covered everything with tests

--- pbhouse/apps.py
+++ pbhouse/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class PbhouseConfig(AppConfig):
     name = 'pbhouse'
+
+    def ready(self):
+        import pbhouse.signals
+
--- pbhouse/tests.py
+++ pbhouse/tests.py
@@ -1,4 +1,8 @@
+from unittest.mock import patch
+
 from django.test import TestCase
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 
 class NotesListTestCase(TestCase):
@@ -7,4 +11,29 @@ class NotesListTestCase(TestCase):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'pbhouse/landing.html')
+
+
+class UserProfileTestCase(TestCase):
+
+    def test_signal(self):
+        
+        with patch(
+            'pbhouse.signals.create_user_profile', 
+            autospec=True) as mocked_handler:
+            post_save.connect(mocked_handler, sender=User)
+
+        with patch(
+            'pbhouse.signals.save_user_profile', 
+            autospec=True) as mocked_handler2:
+            post_save.connect(mocked_handler2, sender=User)
+
+
+
+        user = User.objects.create(
+                                username='editor1', first_name='John', 
+                                last_name='Doe', email='johndoe@gmail.com',
+                                password='142fdasf3D')
+
+        self.assertTrue(mocked_handler.call_count, 1) 
+        self.assertTrue(mocked_handler2.call_count, 1) 
 
