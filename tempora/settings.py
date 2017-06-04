30ec2efb2bdf786f6a1e6ed9606eb3acc1f99b9b
biletboh <biletskyboh@gmail.com>
2017-06-04 16:01:10 +0300

    added translations to the project

--- tempora/settings.py
+++ tempora/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+from django.utils.translation import ugettext_lazy as _
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -44,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -107,6 +110,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+LANGUAGES = (
+        ('en', _('English')),
+        ('uk', _('Ukrainian')),
+        )
+
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
@@ -114,6 +122,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = (
+            os.path.join(BASE_DIR, 'locale'),
+            )
 
 
 # Static files (CSS, JavaScript, Images)
--- tempora/urls.py
+++ tempora/urls.py
@@ -2,11 +2,21 @@
 """
 
 from django.conf.urls import url, include
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('pbhouse.urls'))
-]
+            url(r'^admin/', admin.site.urls),
+            ] + i18n_patterns(
+                            url(r'^', include('pbhouse.urls'))
+                            )
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+                        settings.MEDIA_URL,
+                        document_root=settings.MEDIA_ROOT)
 
