2016b14a1cf520424ce9750ce8fbd4c90f3f919d
biletboh <biletskyboh@gmail.com>
2017-06-18 15:49:26 +0300

    fixed migrations issues and cleared userprofile from pbhouse app

--- pbhouse/apps.py
+++ pbhouse/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class PbhouseConfig(AppConfig):
     name = 'pbhouse'
 
-    def ready(self):
-        import pbhouse.signals
-
--- pbhouse/models.py
+++ pbhouse/models.py
@@ -1,22 +1,2 @@
 from django.db import models
-from django.conf import settings
-from django.contrib.auth.models import User
-
-from easy_thumbnails.fields import ThumbnailerImageField
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(
-                            settings.AUTH_USER_MODEL, unique=True,
-                            primary_key=True, on_delete=models.CASCADE,
-                            related_name='user_profile')
-    avatar = ThumbnailerImageField(upload_to='profile_images', blank=True)
-    info = models.CharField(max_length=512)
-    facebook = models.CharField(max_length=128)
-    twitter = models.CharField(max_length=128)
-    linkedin = models.CharField(max_length=128)
-    approved = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        return self.user.username
 
