d9c840e6ef530d938ea8cc6b3ca929b5a8a0f3cb
biletboh <biletskyboh@gmail.com>
2017-07-29 12:06:48 +0300

    added all basic crud views for blog

--- blog/urls.py
+++ blog/urls.py
@@ -12,5 +12,16 @@ urlpatterns = [
             url(r'^$', views.PostList.as_view(), name='blog'),
             url(r'^blog/(?P<pk>[0-9]+)/$', views.Page.as_view(), name='page'),
             url(r'^post/create/$', views.CreatePost.as_view(), name='create'),
+            url(
+                r'^update/list/$', views.EditPostList.as_view(),
+                name='edit_list'),
+            url(
+                r'^post/update/(?P<pk>[0-9]+)/$',
+                views.EditPost.as_view(),
+                name='edit'),
+            url(
+                r'^post/delete/(?P<pk>[0-9]+)/$',
+                views.DeletePost.as_view(),
+                name='delete'),
             ]
 
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.views.generic import View, DetailView, FormView, TemplateView,\
         DeleteView, ListView 
 from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
+from django.utils.translation import ugettext_lazy as _
+from django.views.generic.detail import SingleObjectMixin
 
 from el_pagination.views import AjaxListView
 
@@ -53,4 +55,119 @@ class CreatePost(SuccessMessageMixin, FormView):
         post.save()
         form.delete_temporary_files()
         return super(CreatePost, self).form_valid(form)
+
+
+class DisplayPost(DetailView):
+    """
+    Get Post for editting.
+    """
+
+    model = Post 
+    template_name = 'blog/edit.html'
+    login_url = reverse_lazy('users:dashboard') 
+
+    def get_context_data(self, **kwargs):
+        context = super(DisplayPost, self).get_context_data(**kwargs)
+
+        initial = {
+                'title': self.object.title,
+                'body': self.object.body,
+                }
+
+        context['form'] = PostForm(initial=initial)
+        return context
+
+
+class UpdatePost(SuccessMessageMixin, SingleObjectMixin, FormView):
+    """
+    Update a Post.
+    """
+
+    form_class = PostForm 
+    model = Post 
+    success_message = _('Пост оновлено успішно.')
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super(UpdatePost, self).post(request, *args, **kwargs)
+ 
+    def form_valid(self, form):
+        params = {'pk': self.object.pk}
+        post = Post.objects.get(**params)
+
+        post.title= form.cleaned_data['title'] 
+        post.body = form.cleaned_data['body'] 
+        image = form.cleaned_data['image']
+
+        if image: 
+            post.image = image
+        post.save()
+
+        form.delete_temporary_files()
+
+        return super(UpdatePost, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('blog:edit', kwargs={'pk': self.object.pk})
+
+
+class EditPost(LoginRequiredMixin, UserPassesTestMixin, View):
+    """
+    Edit post.
+    """
+    
+    login_url = reverse_lazy('users:dashboard') 
+
+    def get(self, request, *args, **kwargs):
+        view = DisplayPost.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = UpdatePost.as_view()
+        return view(request, *args, **kwargs)
+    
+    def test_func(self):
+        user = self.request.user.is_staff
+        if not user:
+            denied = _('У вас немає повноважень, щоб переглядати цю сторінку.')
+            messages.warning(self.request, denied)
+        return user
+
+
+class DeletePost(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """
+    Delete Post.
+    """
+
+    model = Post 
+    success_url = reverse_lazy('blog:edit_list')
+    login_url = reverse_lazy('users:dashboard')
+
+    def test_func(self):
+        user = self.request.user.is_staff
+        if not user:
+            denied = _('У вас немає повноважень, щоб переглядати цю сторінку.')
+            messages.warning(self.request, denied)
+        return user
+
+
+class EditPostList(LoginRequiredMixin, UserPassesTestMixin, AjaxListView):
+    """
+    Render a list of Posts to edit with ajax endless pagination.
+    """
+
+    context_object_name = "posts"
+    template_name = 'blog/edit_list.html'
+    page_template = 'blog/posts.html'
+    login_url = reverse_lazy('users:dashboard')
+
+    def get_queryset(self): 
+        return Post.objects.all()
+    
+    def test_func(self):
+        user = self.request.user.is_staff
+        if not user:
+            denied = _('You have to be a moderator to access this page.')
+            messages.warning(self.request, denied)
+        return user
 
