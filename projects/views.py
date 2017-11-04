cf57a444a0fb977954e5cf7f215e7508fdab03e9
biletboh <biletskyboh@gmail.com>
2017-11-04 21:12:51 +0200

    added create project view

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView, CreateView
+
+from projects.models import Project
 
 
 class ProjectPage(TemplateView):
     """Render projects page."""
 
-    template_name='projects/project_page.html'
+    template_name = 'projects/project_page.html'
+
+
+class ProjectCreate(CreateView):
+    """Create projects."""
+
+    model = Project
+    fields = ('title', 'short_descr', 'description',
+              'image', 'slug', 'curators')
+    template_name = 'projects/create.html'
 
