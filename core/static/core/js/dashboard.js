585a5c75a986ed94ee607d6c6b4a86ba79f23d64
biletboh <biletskyboh@gmail.com>
2018-12-27 16:16:42 +0200

    added jquery validate to dashboard

--- core/static/core/js/dashboard.js
+++ core/static/core/js/dashboard.js
@@ -1,12 +1,26 @@
 $(function () {
+  // change trash icon for ajax selects
+  function prettifyKillicon() {
+    var kill = $('.ui-icon.ui-icon-trash');
+    kill.empty();
+    kill.append('<i class="fa fa-lg fa-trash" aria-hidden="true"></i>');
+    console.log(kill);
+  };
 
-	// change trash icon for ajax selects
-	function prettifyKillicon() {
-		var kill = $('.ui-icon.ui-icon-trash');
-		kill.empty();
-		kill.append('<i class="fa fa-lg fa-trash" aria-hidden="true"></i>');
-		console.log(kill);
-	};
-
-	prettifyKillicon();
+  prettifyKillicon();
 });
+
+// Validation
+function setFormValidation(id){
+  $(id).validate({
+    lang: 'uk',
+    errorPlacement: function(error, element) {
+      $(element).parent('div').addClass('has-error');
+      if (element.is(':checkbox')) {
+        error.insertAfter(element.parent());}
+      else {
+        error.insertAfter(element);
+      }
+    },
+  });
+};
