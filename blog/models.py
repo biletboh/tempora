791bb1fcc638dd1e1e5b86a71830a615a07675e0
biletboh <biletskyboh@gmail.com>
2019-10-23 20:54:29 +0300

    refactored blog models

--- blog/models.py
+++ blog/models.py
@@ -1,37 +1,20 @@
 from django.db import models
-from django.utils import timezone
 
 from easy_thumbnails.fields import ThumbnailerImageField
 from tinymce.models import HTMLField
-from transliterate import slugify
 
-from users.models import UserProfile
+from core.models import AbstractPublication
 from tags.models import Tag
 
 
-class Post(models.Model):
+class Post(AbstractPublication):
     """Store post data for a blog."""
 
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
-    title = models.CharField('Заголовок', max_length=128)
-    short_descr = HTMLField('Короткий опис', max_length=256)
     body = HTMLField('Текст', blank=True)
-    selected = models.BooleanField('Обраний пост', default=False, blank=True)
-    pub_date = models.DateTimeField('Дата публікації',
-                                    default=timezone.now)
     image = ThumbnailerImageField('Світлина', upload_to='photos/blog',
                                   blank=True)
-    slug = models.SlugField('Посилання', unique=True, null=True, blank=True)
 
     class Meta:
         ordering = ('-pub_date',)
         verbose_name_plural = 'posts'
-
-    def __str__(self):
-        return self.title
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from tinymce.models import HTMLField
+from transliterate import slugify
+
+from users.models import UserProfile
+
+
+class AbstractPublication(models.Model):
+    """Abstract class to store general data about publications."""
+
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    title = models.CharField('Заголовок', max_length=128)
+    short_descr = HTMLField('Короткий опис', max_length=256)
+    selected = models.BooleanField('Обраний', default=False, blank=True)
+    pub_date = models.DateTimeField('Дата публікації',
+                                    default=timezone.now)
+    slug = models.SlugField('Посилання', unique=True, null=True, blank=True)
+
+    class Meta:
+        ordering = ('-pub_date',)
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
